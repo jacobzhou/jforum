71ed81154e04adf756d3c88445e060af3feea4ef
Jacob Zhou <jacobz@yeah.net>
2013-11-24 20:25:14 +0800

    fix

--- app/assets/javascripts/jacob.js
+++ app/assets/javascripts/jacob.js
@@ -1,15 +1,15 @@
 $(function(){
 	$(".add_nested_fields").click(function(){
-		add_index_to_fields();
+		setTimeout("add_index_to_fields", 1000);
 	});
 	$(".remove_nested_fields").click(function(){
-		add_index_to_fields();
+		setTimeout("add_index_to_fields", 1000);
 	});
 });
 
 function add_index_to_fields(){
 	$(".simple_form  .fields").each(function(index, e){
-  	$(e).find('label:first').html(index+"、"+$(e).find('label:first').html()); 
+  	$(e).find('label:first').html(index+"、标题"); 
 	});
 }
 
