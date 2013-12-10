1d6ee7bd109df6e7e6d0e386434f8e607979d807
Jacob Zhou <jacobz@yeah.net>
2013-12-10 22:02:03 +0800

    fix

--- app/models/survey_user_answer.rb
+++ app/models/survey_user_answer.rb
@@ -3,6 +3,7 @@ class SurveyUserAnswer < ActiveRecord::Base
   attr_protected
 
   belongs_to :survey_user_sheet
+  belongs_to :survey_question
 
   has_many :photos, :as => :photoable
   accepts_nested_attributes_for :photos, :reject_if => lambda { |item| item[:file].blank? }, :allow_destroy => true
